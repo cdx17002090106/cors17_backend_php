ce452d594797df8a447e3dc1634330a6fee57503
Administrator <1230@qq.com>
2018-12-28 09:03:19 +0800

    1023

--- profile.php
+++ profile.php
@@ -2,5 +2,7 @@
 header("Access-Control-Allow-Origin: http://10.30.29.55:63341");
 header("Access-Control-Allow-Credentials: true");
 $data=array('userName'=>'123456');
+//cdxcdxcdx
 echo json_encode($data);
+
 ?>
